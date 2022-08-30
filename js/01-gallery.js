9b3a555da4b00fc40b5ccd6922a3400ab7e38e9a
Valeriia-Livinska <valerylivi@gmail.com>
2022-08-30 21:23:24 +0300

    shorten var

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -26,49 +26,75 @@ function createGalleryMarkup(items) {
 const galleryEl = document.querySelector(".gallery");
 galleryEl.insertAdjacentHTML("beforeend", markup);
 
-galleryEl.addEventListener("click", cancelDefaultFollowing);
 galleryEl.addEventListener("click", onGalleryElemClick);
 
-function cancelDefaultFollowing(event) {
-  event.preventDefault();
-}
-
 function onGalleryElemClick(event) {
+  event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
   const urlLargePhoto = event.target.dataset.source;
 
-  const openModal = basicLightbox.create(`<img src="${urlLargePhoto}">`);
+  const openModal = basicLightbox.create(`<img src="${urlLargePhoto}">`, {
+    onShow: () => {
+      window.addEventListener("keydown", closeWithEscButton);
+    },
+    onClose: () => {
+      window.removeEventListener("keydown", closeWithEscButton);
+    },
+  });
   openModal.show();
-
-  const modal = document.querySelector(".basicLightbox");
-
-  window.addEventListener("keydown", closeWithEscButton);
-  console.log("keydown listener was added");
-  modal.addEventListener("click", closeWithClick);
-  console.log("click listener was added");
 
   function closeWithEscButton(event) {
     if (event.code === "Escape") {
       openModal.close();
-      window.removeEventListener("keydown", closeWithEscButton);
-      console.log("keydown event listener was removed from window");
-      modal.removeEventListener("click", closeWithClick);
-      console.log("click event listener was removed from modal");
     }
-  }
-
-  function closeWithClick(event) {
-    if (openModal.close()) {
-      removeListenerOnClick();
-    }
-  }
-
-  function removeListenerOnClick() {
-    modal.removeEventListener("click", closeWithClick);
-    console.log("click event listener was removed from modal");
-    window.removeEventListener("keydown", closeWithEscButton);
-    console.log("keydown event listener was removed from window");
   }
 }
+
+// galleryEl.addEventListener("click", cancelDefaultFollowing);
+// galleryEl.addEventListener("click", onGalleryElemClick);
+
+// function cancelDefaultFollowing(event) {
+//   event.preventDefault();
+// }
+
+// function onGalleryElemClick(event) {
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   const urlLargePhoto = event.target.dataset.source;
+
+//   const openModal = basicLightbox.create(`<img src="${urlLargePhoto}">`);
+//   openModal.show();
+
+//   const modal = document.querySelector(".basicLightbox");
+
+//   window.addEventListener("keydown", closeWithEscButton);
+//   console.log("keydown listener was added");
+//   modal.addEventListener("click", closeWithClick);
+//   console.log("click listener was added");
+
+//   function closeWithEscButton(event) {
+//     if (event.code === "Escape") {
+//       openModal.close();
+//       window.removeEventListener("keydown", closeWithEscButton);
+//       console.log("keydown event listener was removed from window");
+//       modal.removeEventListener("click", closeWithClick);
+//       console.log("click event listener was removed from modal");
+//     }
+//   }
+
+//   function closeWithClick(event) {
+//     if (openModal.close()) {
+//       removeListenerOnClick();
+//     }
+//   }
+
+//   function removeListenerOnClick() {
+//     modal.removeEventListener("click", closeWithClick);
+//     console.log("click event listener was removed from modal");
+//     window.removeEventListener("keydown", closeWithEscButton);
+//     console.log("keydown event listener was removed from window");
+//   }
+// }
